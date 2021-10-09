0bf103e3aaadd86308e17ea9d996ea267b2b4e39
kcringler <92175614+kcringler@users.noreply.github.com>
2021-10-09 11:34:39 +0200

    Create echo.cpp

---
+++ echo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std; // substitution for cin & cout
+
+int main() // initialize the main loop to start the program
+{
+        int number; // initialize the variable 'number'
+        cin >> number; // capture input and pass to variable
+        cout << number; // print output from variable to screen
+        return 0; // end the program
+}
